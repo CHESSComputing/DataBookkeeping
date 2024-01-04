f82e2cdce4097bd65be85d4254f5a8d1d3c3bc30
Valentin Kuznetsov <vkuznet@gmail.com>
2024-01-04 13:45:14 -0500

    schema updates

--- static/schema/mysql.sql
+++ static/schema/mysql.sql
@@ -1,34 +1,65 @@
-CREATE TABLE cycles (
- cycle_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
- name VARCHAR(100) NOT NULL UNIQUE
+CREATE TABLE processing (
+    processing_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    processing VARCHAR(255) NOT NULL UNIQUE,
+    create_at INTEGER,
+    create_by VARCHAR(255),
+    modify_at INTEGER,
+    modify_by VARCHAR(255)
 );
-
-CREATE TABLE beamlines (
- beamline_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
- name VARCHAR(100) NOT NULL UNIQUE
+CREATE TABLE parents (
+    parent_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    parent VARCHAR(255) NOT NULL UNIQUE,
+    create_at INTEGER,
+    create_by VARCHAR(255),
+    modify_at INTEGER,
+    modify_by VARCHAR(255)
 );
-
-CREATE TABLE btrs (
- btr_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
- name VARCHAR(100) NOT NULL UNIQUE
+CREATE TABLE site (
+    site_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    site VARCHAR(255) NOT NULL UNIQUE,
+    create_at INTEGER,
+    create_by VARCHAR(255),
+    modify_at INTEGER,
+    modify_by VARCHAR(255)
 );
-
-CREATE TABLE samples (
- sample_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
- name VARCHAR(100) NOT NULL UNIQUE
+CREATE TABLE buckets (
+    bucket_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    bucket VARCHAR(255) NOT NULL UNIQUE,
+    meta_id BIGINT REFERENCES metadata(meta_id) ON UPDATE CASCADE,
+    dataset_id BIGINT REFERENCES datasets(dataset_id) ON UPDATE CASCADE,
+    create_at INTEGER,
+    create_by VARCHAR(255),
+    modify_at INTEGER,
+    modify_by VARCHAR(255)
 );
-
+CREATE TABLE metadata (
+    meta_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    did VARCHAR(255) NOT NULL UNIQUE,
+    create_at INTEGER,
+    create_by VARCHAR(255),
+    modify_at INTEGER,
+    modify_by VARCHAR(255)
+);
 CREATE TABLE datasets (
- dataset_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
- cycle_id INTEGER REFERENCES cycles(cycle_id) ON UPDATE CASCADE,
- beamline_id INTEGER REFERENCES beamlines(beamline_id) ON UPDATE CASCADE,
- btr_id INTEGER REFERENCES btrs(btr_id) ON UPDATE CASCADE,
- sample_id INTEGER REFERENCES sample(sample_id) ON UPDATE CASCADE,
- tstamp INTEGER NOT NULL UNIQUE
+    dataset_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dataset VARCHAR(255) NOT NULL UNIQUE,
+    meta_id BIGINT REFERENCES metadata(meta_id) ON UPDATE CASCADE,
+    site_id BIGINT REFERENCES site(site_id) ON UPDATE CASCADE,
+    processing_id BIGINT REFERENCES processingS(processing_id) ON UPDATE CASCADE,
+    parent_id BIGINT REFERENCES parents(parent_id) ON UPDATE CASCADE,
+    create_at INTEGER,
+    create_by VARCHAR(255),
+    modify_at INTEGER,
+    modify_by VARCHAR(255)
 );
-
 CREATE TABLE files (
- file_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
- dataset_id BIGINT REFERENCES datasets(dataset_id) ON UPDATE CASCADE,
- name VARCHAR(100) NOT NULL
+    file_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    file VARCHAR(255) NOT NULL UNIQUE,
+    is_file_valid INTEGER DEFAULT 1,
+    meta_id BIGINT REFERENCES metadata(meta_id) ON UPDATE CASCADE,
+    dataset_id BIGINT REFERENCES datasets(dataset_id) ON UPDATE CASCADE,
+    create_at INTEGER,
+    create_by VARCHAR(255),
+    modify_at INTEGER,
+    modify_by VARCHAR(255)
 );
--- static/schema/sqlite.sql
+++ static/schema/sqlite.sql
@@ -1,37 +1,67 @@
 --https://stackoverflow.com/questions/18387209/sqlite-syntax-for-creating-table-with-foreign-key
 
-CREATE TABLE cycles (
- cycle_id INTEGER PRIMARY KEY AUTOINCREMENT,
- name TEXT NOT NULL UNIQUE
+create TABLE processing (
+    processing_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    processing VARCHAR(255) NOT NULL UNIQUE,
+    create_at INTEGER,
+    create_by VARCHAR(255),
+    modify_at INTEGER,
+    modify_by VARCHAR(255)
 );
-
-CREATE TABLE beamlines (
- beamline_id INTEGER PRIMARY KEY AUTOINCREMENT,
- name TEXT NOT NULL UNIQUE
+create TABLE parents (
+    parent_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    parent VARCHAR(255) NOT NULL UNIQUE,
+    create_at INTEGER,
+    create_by VARCHAR(255),
+    modify_at INTEGER,
+    modify_by VARCHAR(255)
 );
-
-CREATE TABLE btrs (
- btr_id INTEGER PRIMARY KEY AUTOINCREMENT,
- name TEXT NOT NULL UNIQUE
+create TABLE sites (
+    site_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    site VARCHAR(255) NOT NULL UNIQUE,
+    create_at INTEGER,
+    create_by VARCHAR(255),
+    modify_at INTEGER,
+    modify_by VARCHAR(255)
 );
-
-CREATE TABLE samples (
- sample_id INTEGER PRIMARY KEY AUTOINCREMENT,
- name TEXT NOT NULL UNIQUE
+create TABLE buckets (
+    bucket_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    bucket VARCHAR(255) NOT NULL UNIQUE,
+    meta_id BIGINT REFERENCES metadata(meta_id) ON UPDATE CASCADE,
+    dataset_id INTEGER REFERENCES datasets(dataset_id) ON UPDATE CASCADE,
+    create_at INTEGER,
+    create_by VARCHAR(255),
+    modify_at INTEGER,
+    modify_by VARCHAR(255)
 );
-
-CREATE TABLE datasets (
- dataset_id INTEGER PRIMARY KEY,
- cycle_id integer REFERENCES cycles(cycle_id) ON UPDATE CASCADE,
- beamline_id integer REFERENCES beamlines(beamline_id) ON UPDATE CASCADE,
- btr_id integer REFERENCES btrs(btr_id) ON UPDATE CASCADE,
- sample_id integer REFERENCES sample(sample_id) ON UPDATE CASCADE,
- tstamp INTEGER NOT NULL UNIQUE
+create TABLE metadata (
+    meta_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    did VARCHAR(255) NOT NULL UNIQUE,
+    create_at INTEGER,
+    create_by VARCHAR(255),
+    modify_at INTEGER,
+    modify_by VARCHAR(255)
 );
-
-CREATE TABLE files (
- file_id INTEGER PRIMARY KEY AUTOINCREMENT,
- dataset_id INTEGER REFERENCES datasets(dataset_id) ON UPDATE CASCADE,
- name TEXT NOT NULL
+create TABLE datasets (
+    dataset_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    dataset VARCHAR(255) NOT NULL UNIQUE,
+    meta_id INTEGER REFERENCES metadata(meta_id) ON UPDATE CASCADE,
+    site_id INTEGER REFERENCES sites(site_id) ON UPDATE CASCADE,
+    processing_id INTEGER REFERENCES processingS(processing_id) ON UPDATE CASCADE,
+    parent_id INTEGER REFERENCES parents(parent_id) ON UPDATE CASCADE,
+    create_at INTEGER,
+    create_by VARCHAR(255),
+    modify_at INTEGER,
+    modify_by VARCHAR(255)
 );
-
+create TABLE files (
+    file_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    file VARCHAR(255) NOT NULL UNIQUE,
+    is_file_valid INTEGER DEFAULT 1,
+    meta_id INTEGER REFERENCES metadata(meta_id) ON UPDATE CASCADE,
+    dataset_id INTEGER REFERENCES datasets(dataset_id) ON UPDATE CASCADE,
+    create_at INTEGER,
+    create_by VARCHAR(255),
+    modify_at INTEGER,
+    modify_by VARCHAR(255)
+);
