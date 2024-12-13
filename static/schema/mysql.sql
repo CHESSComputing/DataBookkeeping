59708c538ac94533bda0ec09465d1d3def3a5aa5
Valentin Kuznetsov <vkuznet@gmail.com>
2024-12-13 13:02:14 -0500

    expand schemas to include python and os environments, as well as scripts

--- static/schema/mysql.sql
+++ static/schema/mysql.sql
@@ -1,11 +1,15 @@
 CREATE TABLE processing (
     processing_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     processing VARCHAR(255) NOT NULL UNIQUE,
+    environment_id BIGINT REFERENCES python_environments(environment_id) ON UPDATE CASCADE,
+    os_id BIGINT REFERENCES os_environments(os_id) ON UPDATE CASCADE,
+    script_id BIGINT REFERENCES scripts(script_id) ON UPDATE CASCADE,
     create_at INTEGER,
     create_by VARCHAR(255),
     modify_at INTEGER,
     modify_by VARCHAR(255)
 );
+
 CREATE TABLE parents (
     parent_id INTEGER NOT NULL,
     dataset_id INTEGER NOT NULL,
@@ -14,6 +18,7 @@ CREATE TABLE parents (
     modify_at INTEGER,
     modify_by VARCHAR(255)
 );
+
 CREATE TABLE sites (
     site_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     site VARCHAR(255) NOT NULL UNIQUE,
@@ -22,6 +27,7 @@ CREATE TABLE sites (
     modify_at INTEGER,
     modify_by VARCHAR(255)
 );
+
 CREATE TABLE buckets (
     bucket_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     bucket VARCHAR(255) NOT NULL UNIQUE,
@@ -31,22 +37,59 @@ CREATE TABLE buckets (
     modify_at INTEGER,
     modify_by VARCHAR(255)
 );
+
 CREATE TABLE datasets (
     dataset_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     did VARCHAR(255) NOT NULL UNIQUE,
-    site_id BIGINT REFERENCES site(site_id) ON UPDATE CASCADE,
-    processing_id BIGINT REFERENCES processingS(processing_id) ON UPDATE CASCADE,
+    site_id BIGINT REFERENCES sites(site_id) ON UPDATE CASCADE,
+    processing_id BIGINT REFERENCES processing(processing_id) ON UPDATE CASCADE,
     parent_id BIGINT REFERENCES parents(parent_id) ON UPDATE CASCADE,
     create_at INTEGER,
     create_by VARCHAR(255),
     modify_at INTEGER,
     modify_by VARCHAR(255)
 );
+
 CREATE TABLE files (
     file_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     file VARCHAR(255) NOT NULL UNIQUE,
     is_file_valid INTEGER DEFAULT 1,
     dataset_id BIGINT REFERENCES datasets(dataset_id) ON UPDATE CASCADE,
+    create_at INTEGER,
+    create_by VARCHAR(255),
+    modify_at INTEGER,
+    modify_by VARCHAR(255)
+);
+
+-- Python Environments
+CREATE TABLE python_environments (
+    environment_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,        -- Name of the environment (e.g., conda, virtualenv)
+    version VARCHAR(255),              -- Python version
+    details TEXT,                      -- Additional environment details
+    create_at INTEGER,
+    create_by VARCHAR(255),
+    modify_at INTEGER,
+    modify_by VARCHAR(255)
+);
+
+-- OS Environments
+CREATE TABLE os_environments (
+    os_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    os_name VARCHAR(255) NOT NULL,     -- Operating system name (e.g., Linux)
+    os_version VARCHAR(255),           -- Kernel/OS version
+    release_number VARCHAR(255),       -- OS release number
+    create_at INTEGER,
+    create_by VARCHAR(255),
+    modify_at INTEGER,
+    modify_by VARCHAR(255)
+);
+
+-- Scripts
+CREATE TABLE scripts (
+    script_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    script_name VARCHAR(255) NOT NULL, -- Name of the script
+    parameters TEXT,                   -- Parameters used for the script
     create_at INTEGER,
     create_by VARCHAR(255),
     modify_at INTEGER,
--- static/schema/sqlite.sql
+++ static/schema/sqlite.sql
@@ -3,6 +3,9 @@
 create TABLE processing (
     processing_id INTEGER PRIMARY KEY AUTOINCREMENT,
     processing VARCHAR(255) NOT NULL UNIQUE,
+    environment_id INTEGER REFERENCES python_environments(environment_id) ON UPDATE CASCADE,
+    os_id INTEGER REFERENCES os_environments(os_id) ON UPDATE CASCADE,
+    script_id INTEGER REFERENCES scripts(script_id) ON UPDATE CASCADE,
     create_at INTEGER,
     create_by VARCHAR(255),
     modify_at INTEGER,
@@ -49,6 +52,35 @@ create TABLE files (
     file VARCHAR(255) NOT NULL UNIQUE,
     is_file_valid INTEGER DEFAULT 1,
     dataset_id INTEGER REFERENCES datasets(dataset_id) ON UPDATE CASCADE,
+    create_at INTEGER,
+    create_by VARCHAR(255),
+    modify_at INTEGER,
+    modify_by VARCHAR(255)
+);
+CREATE TABLE python_environments (
+    environment_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL,
+    version VARCHAR(255),
+    details TEXT,
+    create_at INTEGER,
+    create_by VARCHAR(255),
+    modify_at INTEGER,
+    modify_by VARCHAR(255)
+);
+CREATE TABLE os_environments (
+    os_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    os_name VARCHAR(255) NOT NULL,
+    os_version VARCHAR(255),
+    release_number VARCHAR(255),
+    create_at INTEGER,
+    create_by VARCHAR(255),
+    modify_at INTEGER,
+    modify_by VARCHAR(255)
+);
+CREATE TABLE scripts (
+    script_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    script_name VARCHAR(255) NOT NULL,
+    parameters TEXT,
     create_at INTEGER,
     create_by VARCHAR(255),
     modify_at INTEGER,
