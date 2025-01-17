6cbade5890d3694b04aa374670d8f7ff2f3fcbf2
Valentin Kuznetsov <vkuznet@gmail.com>
2025-01-17 11:22:01 -0500

    New module to implement manytomany functions

---
+++ dbs/manytomany.go
@@ -0,0 +1,12 @@
+package dbs
+
+// TODO: implement manytomany functions
+func InsertDatasetFile(datasetId, fileId int64, ftype string) error {
+	return nil
+}
+func InsertDatasetEnvironment(datasetId, envId int64) error {
+	return nil
+}
+func InsertEnvironmentPackage(datasetId, pkgId int64) error {
+	return nil
+}
