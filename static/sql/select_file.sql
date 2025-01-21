556e9479c79bcca5b963068d5399338a4f5bfd61
Valentin Kuznetsov <vkuznet@gmail.com>
2025-01-21 10:27:13 -0500

    Adjust sql statment to properly join datasets_files table

--- static/sql/select_file.sql
+++ static/sql/select_file.sql
@@ -7,5 +7,5 @@ SELECT
     F.modify_by,
     F.modify_at
 FROM files F
-JOIN datasets D on D.dataset_id = F.dataset_id
-LEFT JOIN datasets_files DF ON D.dataset_id = DF.dataset_id
+LEFT JOIN datasets_files DF ON DF.file_id = F.file_id
+LEFT JOIN datasets D on D.dataset_id = DF.dataset_id
